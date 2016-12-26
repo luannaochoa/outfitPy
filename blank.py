58f55e8acc6f6477832f67985c55d68309f4d61a
luannaochoa <locho01@fiu.edu>
2016-12-26 16:22:44 -0500

    updated py

--- blank.py
+++ blank.py
@@ -1,5 +1,12 @@
 #!/use/bin/python
-import sqlite3 
+import sqlite3
+
+####FUNCTIONS DEFINED 
+##create table
+##insert data
+##update look
+##remove look 
+##show all looks 
 
 def create_table(db_Name,table_name, sql):
     with sqlite3.connect(db_Name) as db: 
@@ -10,7 +17,6 @@ def create_table(db_Name,table_name, sql):
         if len(result) == 1:
             response = input("The table {0} already exists, do you wish to recreate it (y/n): ".format(table_name))
             if response == "y":
-                print 3
                 keep_table = False
                 print ("The {0} table will be recreated - all existing data will be lost.".format(table_name))
                 cursor.execute("drop table if exists {0}".format(table_name))
@@ -26,17 +32,77 @@ def create_table(db_Name,table_name, sql):
 def insert_data(db_Name, values):
     with sqlite3.connect(db_Name) as db:
         cursor = db.cursor()
-        sql = "insert into Clothing (Style, Top, Bottom, Hair, Shoes ,Colors, Notes)  values (?,?,?,?,?,?,?)"
+        sql = "insert into Looks (Style, Top, Bottom, Shoes, Hair ,Colors, Notes)  values (?,?,?,?,?,?,?)"
         cursor.execute(sql, values)
         db.commit()
         
+def update_look(db_Name, data):
+    with sqlite3.connect(db_Name) as db:
+        cursor = db.cursor()
+        sql = "update Looks set Style = ?, Top = ?, Bottom = ?, Shoes = ?, Hair = ?, Colors = ?, Notes = ?"
+        cursor.execute(sql, data)
+        db.commit() 
+
+def remove_look(db_Name, data):
+    with sqlite3.connect(db_Name) as db:
+        cursor = db.cursor()
+        sql ="delete from Looks where Style = ?"
+        cursor.execute(sql,data)
+        db.commit()
+
+def query(db_name, sql, data):
+    with sqlite3.connect(db_name) as db:
+        cursor = db.cursor()
+        cursor.execute("PRAGMA Foreign_Keys = ON")
+        cursor.execute(sql, data)
+        result = cursor.fetchall()
+        db.commit()
+        return result
+
+def show_all_looks(db_Name, table_name):
+    "show all the data of table from db"
+    sql = "SELECT * FROM {0}".format(table_name)
+    return query(db_Name, sql, ())
+
+
+def print_table(result):
+    "show the data in a table"
+    print("""
+        |{0:^5s}|{1:^13s}|{2:^10s}|{3:^10s}|{4:^10s}|{5:^12s}|{6:^20s}|{7:^20s}|
+        """.format("Outfit ID",
+                   "Style",
+                   "Top",
+                   "Bottom",
+                   "Shoes",
+                   "Hair",
+                   "Colors",
+                   "Notes"))
+    for entry in result:
+        outfitID, Style, Top, Bottom, Shoes, Hair, Colors, Notes = entry
+        print("""
+        {0:^5d} {1:^13s} {2:^10s} {3:^10s} {4:^10s} {5:^12s} {6:^20s} {7:^20s}
+            """.format(outfitID,
+                       Style,
+                       Top,
+                       Bottom,
+                       Shoes,
+                       Hair,
+                       Colors,
+                       Notes))
+
 
 if __name__ == "__main__":
-    db_Name = "ex.db"
-    sql = """create table Clothing(ClothingID text,Style text,Color text, size text,primary key(ClothingID))"""
-    create_table(db_Name, "Clothing",sql)
-    clothing = ("Shirt", "Pink", "Small")
-    insert_data(db_Name, clothing)
+    db_Name = "looks_inventory.db"
+    sql = """create table Looks(outfitID integer,Style text, Top text, Bottom text, Shoes text, Hair text, Colors text, Notes text, primary key(outfitID))"""
+    create_table(db_Name, "Looks",sql)
+    look = ("Casual", "Black Turtle Neck Elbow Length", "Brown Cargo Style", "Black Roshe Nikes", "Bun", "Black, Brown", "Safari look")
+    insert_data(db_Name, look)
+    data = ("Casual Attire", "Black Turtle Neck Elbow Length", "Brown Cargo Style", "Black Roshe Nikes", "Bun", "Black, Brown", "Safari look")
+    update_look(db_Name, data)
+    result = show_all_looks(db_Name, "Looks")
+    print_table(result)
+
+
 
 
 
